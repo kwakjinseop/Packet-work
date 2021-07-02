ef4f36b8e4b141db519735cbe3523b9c11f4f8ca
kwakjinseop <66795577+kwakjinseop@users.noreply.github.com>
2021-07-02 17:30:14 +0900

    7.2 수정
    7.2 수정

--- PYQT/Main.py
+++
@@ -1,8 +0,0 @@
-import sys
-import PyQt5.QtWidgets as qtwid
-from MainWindow import MainWindow
-
-app = qtwid.QApplication(sys.argv)
-mw = MainWindow()
-mw.show()
-app.exec()
--- PYQT/test.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-if __name__ == "__main__":
-    print("Start")
-
-    cur_time = datetime.datetime.now()
-    print("Current time : %s" % cur_time)
-
-    print("End")
